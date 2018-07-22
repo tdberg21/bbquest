a118963e9ff5035524696839a45a30bd33e7804e
Tory Dannenberg <rfdberg@gmail.com>
2018-07-22 12:30:10 -0600

    Add id to return state

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,7 +1,7 @@
 export const userReducer = (state={}, action) => {
   switch (action.type) {
     case 'LOGIN_USER':
-      return { username: action.username };
+      return { username: action.username, id: action.id };
     default:
       return state;
   }
