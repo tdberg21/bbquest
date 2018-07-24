fca7e6717fc639bd7b17ee0c16f19cd00bc0ed65
Tory Dannenberg <rfdberg@gmail.com>
2018-07-23 20:47:07 -0600

    Add link to restaurant/restaurant.name in card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Card.css';
+import { Link } from 'react-router-dom';
+
 
 const Card = (props) => {
   return(
     <div className="card">
-      <h3>{props.name}</h3>
-      <img src={props.image} alt="restaurant picture" height="200px"/>
+      <Link to={`/restaurant/${props.name}`} >
+        <h3>{props.name}</h3>
+        <img src={props.image} alt="restaurant picture" height="200px"/>
+      </Link>
     </div>
   );
 };
