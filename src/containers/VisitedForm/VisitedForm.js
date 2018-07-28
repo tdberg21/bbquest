3d935b3dcad3b290131cc2afee606c0d1fe894a5
Tory Dannenberg <rfdberg@gmail.com>
2018-07-28 12:21:40 -0600

    :ambulance: Fix conditional rendering to redirect user to sign in if they try to save visited as guest

--- src/containers/VisitedForm/VisitedForm.js
+++ src/containers/VisitedForm/VisitedForm.js
@@ -29,63 +29,62 @@ export class VisitedForm extends Component {
   }
 
   render() {
-    if (this.props.user) {
+    if (this.props.user.username) {
       return (
-      <div className='visited-form-container'>
-        <form className='visited-form'>
-          <div className='inputs'>
-            <input
-              className='meal-field input-fields'
-              aria-label='Please Enter What You Ate'
-              placeholder='What did you eat?'
-              type='text'
-              name='meal'
-              value={this.state.meal}
-              onChange={this.handleChange}
-            />
-            <input
-              className='rating-field input-fields'
-              aria-label='Please Enter a rating out of 10'
-              placeholder='Enter your rating!'
-              type='number'
-              name='rating'
-              value={this.state.rating}
-              onChange={this.handleChange}
-            />
-            <input
-              className='notes-field input-fields'
-              aria-label='Please Enter Notes About Your Visit'
-              placeholder='Notes'
-              type='text'
-              name='notes'
-              value={this.state.notes}
-              onChange={this.handleChange}
-            />
-            <input
-              className='date-field input-fields'
-              aria-label='Please Enter Your Password'
-              placeholder='date of visit'
-              type='date'
-              name='date'
-              value={this.state.date}
-              onChange={this.handleChange}
-            />
-            <button
-              className='visit-button'
-              aria-label='Submit review'
-              onClick={this.handleSubmit}
-            >
-              Submit
-            </button>
-          </div>
-        </form>
-      </div>
-    );
-
+        <div className='visited-form-container'>
+          <form className='visited-form'>
+            <div className='inputs'>
+              <input
+                className='meal-field input-fields'
+                aria-label='Please Enter What You Ate'
+                placeholder='What did you eat?'
+                type='text'
+                name='meal'
+                value={this.state.meal}
+                onChange={this.handleChange}
+              />
+              <input
+                className='rating-field input-fields'
+                aria-label='Please Enter a rating out of 10'
+                placeholder='Enter your rating!'
+                type='number'
+                name='rating'
+                value={this.state.rating}
+                onChange={this.handleChange}
+              />
+              <input
+                className='notes-field input-fields'
+                aria-label='Please Enter Notes About Your Visit'
+                placeholder='Notes'
+                type='text'
+                name='notes'
+                value={this.state.notes}
+                onChange={this.handleChange}
+              />
+              <input
+                className='date-field input-fields'
+                aria-label='Please Enter Your Password'
+                placeholder='date of visit'
+                type='date'
+                name='date'
+                value={this.state.date}
+                onChange={this.handleChange}
+              />
+              <button
+                className='visit-button'
+                aria-label='Submit review'
+                onClick={this.handleSubmit}
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      );
     } else {
       return (
         <div> Please Sign In! </div>
-      )
+      );
     }
 
   }
