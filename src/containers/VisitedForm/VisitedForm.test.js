5a8320ff28c86cf683a9d4e0e15600f6e70d979f
Tory Dannenberg <rfdberg@gmail.com>
2018-07-30 09:14:18 -0600

    Add tests for remaining uncovered lines in Visited Form

--- src/containers/VisitedForm/VisitedForm.test.js
+++ src/containers/VisitedForm/VisitedForm.test.js
@@ -3,10 +3,40 @@ import { shallow } from 'enzyme';
 import { VisitedForm } from './VisitedForm';
 
 describe('VISITED FORM TESTS', () => {
-  it('should match the snapshot', () => {
+  it('should match the snapshot with a user', () => {
     const mockUser = {username: 'Tory'};
     const wrapper = shallow(<VisitedForm user={mockUser}/>);
 
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should match the snapshot without a user', () => {
+    const mockUser = { username: '' };
+    const wrapper = shallow(<VisitedForm user={mockUser} />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should update state when handleChange is invoked', () => {
+    const mockUser = { username: 'Tory' };
+    const wrapper = shallow(<VisitedForm user={mockUser} />);
+    const mockEvent = {target: {name: 'meal', value: 'pulled pork' }};
+
+    wrapper.instance().handleChange(mockEvent);
+
+    expect(wrapper.state('meal')).toEqual('pulled pork');
+    
+  });
+
+  it('should call addRestaurant to Database when handleSubmit is invoked', () => {
+    const mockUser = { username: 'Tory' };
+    const mockAddRestaurant = jest.fn();
+    const mockHistory = {goBack: jest.fn()};
+    const wrapper = shallow(<VisitedForm user={mockUser} addRestaurantToDatabase={mockAddRestaurant} history={mockHistory}/>);
+    const mockEvent = {preventDefault: jest.fn()};
+    
+    wrapper.instance().handleSubmit(mockEvent);
+
+    expect(mockAddRestaurant).toHaveBeenCalled();
   });
 });
